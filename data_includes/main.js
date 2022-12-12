6c8ed6b54d3e4849c584b34b783f2d4d566bc64c
yasufmg <101657118+yasufmg@users.noreply.github.com>
2022-12-11 21:19:33 -0300

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,4 +1,4 @@
-//Inativa os comandos do IBex e sem eles os códigos não funciona
+//Inativa os comandos do IBex e sem eles o código não funciona
 PennController.ResetPrefix(null); // Shorten command names (keep this line here))
 PennController.DebugOff();
 
@@ -157,7 +157,7 @@ newTrial("participants",
 
 // Instrucoes
 newTrial("instructions",
-    newText("instructions_greeting", "<h2>INSTRUÇÕES</h2><p>Neste experimento, você deverá avaliar algumas frases do português de acordo com uma escala de 0 a 100, em que 0 significa TOTALMENTE ARTIFICIAL e 100 significa TOTALMENTE NATURAL.</p><p>Ao iniciar, você verá uma frase. Leia-a com atenção e atribua uma nota a ela movimentando a escala de 0 a 100.</p><p>É importante que você clique e arraste até a nota desejada.</p><p>Por fim, clique em PRÓXIMO para enviar sua nota e continuar avaliando as próximas frases.</p><p>Observe que não nos interessa saber se a frase é correta ou não, mas apenas se ela lhe parece natural ou artificial segundo o uso cotidiano que todos os falantes fazem da língua.</p><p>Após entender essas instruções, clique em INICIAR para começar.</p>")
+    newText("instructions_greeting", "<h2>INSTRUÇÕES</h2><p>Neste experimento, você deverá avaliar algumas frases do português de acordo com uma escala de 0 a 100, em que 0 significa TOTALMENTE ARTIFICIAL e 100 significa TOTALMENTE NATURAL.</p><p>Ao iniciar, você verá uma frase. Leia-a com atenção e atribua uma nota a ela movimentando a escala de 0 a 100.</p><p>É importante que você clique e arraste até a nota desejada.</p><p>Clique em CONTINUAR para enviar sua nota e responda a pergunta. Por fim, clique em PRÓXIMO para continuar avaliando as próximas frases.</p><p>Observe que não nos interessa saber se a frase é correta ou não, mas apenas se ela lhe parece natural ou artificial segundo o uso cotidiano que todos os falantes fazem da língua.</p><p>Após entender essas instruções, clique em INICIAR para começar.</p>")
         .left()
         .cssContainer({"margin":"1em"})
         .print()
@@ -173,31 +173,12 @@ newTrial("instructions",
 // Treinamento
 Template("exercise.csv", row =>
     newTrial( "exercise" ,
-        newText("context", row.CONTEXT)
+        newText("sentence", row.SENTENCE)
             .cssContainer({"margin-top":"2em", "margin-bottom":"2em", "font-size":"1.55em"})
             .center()
             .print()
             ,
-        newButton("go_to_exercise", "Continuar")
-            .cssContainer({"margin":"1em"})
-            .center()
-            .print()
-            .wait()
-            ,
-    // Timer para mostrar nova sentenca
-        newTimer("wait", 800)
-            .start()
-            .wait()
-            ,
-            clear()
-            ,
-            
-        newText("sentence", row.SENTENCE)
-            .cssContainer({"margin-top":"2em", "margin-bottom":"2em", "font-size":"1.5em"})
-            .center()
-            .print()
-            ,
-            
+
     newScale("slider", 100)
         .before( newText("left", "<div class='fancy'> TOTALMENTE ARTIFICIAL (0) </div>") )
         .after( newText("right", "<div class='fancy'> (100) TOTALMENTE NATURAL </div>") )
@@ -217,7 +198,8 @@ Template("exercise.csv", row =>
     .css({display: 'flex', 'justify-content': 'space-between', width: '20em'})
     .center()
     .print()
-    ,
+    	,
+	     
         newButton("go_to_exercise", "Continuar")
         .cssContainer({"margin":"1em"})
         .center()
@@ -256,34 +238,12 @@ newTrial( "start_experiment" ,
 
 Template("experiment.csv", row =>
     newTrial( "experiment-"+row.TYPE,
-        newText("context", row.CONTEXT)
+        newText("sentence", row.SENTENCE)
             .cssContainer({"margin-top":"2em", "margin-bottom":"2em", "font-size":"1.2em"})
             .center()
             .print()
             ,
-        
-        newButton("go_to_exercise", "Continuar")
-        .cssContainer({"margin":"1em"})
-        .center()
-        .print()
-        .wait()
-        ,
-    // Timer para mostrar nova sentenca
-        newTimer("wait", 800)
-            .start()
-            .wait()
-        ,
-    // Limpa a pagina
-        clear()
-        ,
-
-    // Escala
-        newText("sentence", row.SENTENCE)
-            .cssContainer({"margin-top":"2em", "margin-bottom":"2em", "font-size":"1.5em"})
-            .center()
-            .print()
-            ,
-
+	     
     newScale("slider", 100)
         .before( newText("left", "<div class='fancy'> TOTALMENTE ARTIFICIAL (0) </div>") )
         .after( newText("right", "<div class='fancy'> (100) TOTALMENTE NATURAL </div>") )
